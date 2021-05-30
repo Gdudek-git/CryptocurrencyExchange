1c549bd67c9edca4f71a7378b1004f1bc50391b8
Grzegorz Dudek <74261917+MeZziK@users.noreply.github.com>
2021-05-30 14:36:19 +0200

    finished LoginView UI

--- src/main/java/ui/controllers/LoginViewController.java
+++ src/main/java/ui/controllers/LoginViewController.java
@@ -1,6 +1,26 @@
 package main.java.ui.controllers;
 
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class LoginViewController
 {
+    @FXML
+    private TextField tfUsername;
 
+    @FXML
+    private TextField tfPassword;
+
+    @FXML
+    void btnExitOnAction(ActionEvent event) {
+        Platform.exit();
+    }
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event) {
+
+    }
 }
