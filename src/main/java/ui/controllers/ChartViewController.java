13c70791d8e0368aa643acc7f07428c903ae4424
Grzegorz Dudek <74261917+MeZziK@users.noreply.github.com>
2021-05-30 22:57:45 +0200

    renamed ChartView to UserView and added menu to it

--- src/main/java/ui/controllers/ChartViewController.java
+++
@@ -1,6 +0,0 @@
-package main.java.ui.controllers;
-
-public class ChartViewController
-{
-
-}
---
+++ src/main/java/ui/controllers/UserViewController.java
@@ -0,0 +1,48 @@
+package main.java.ui.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+
+public class UserViewController
+{
+    //region javafx controls
+    @FXML
+    private Label lbProfit_Loss;
+
+    @FXML
+    private Label lbBalance;
+
+    @FXML
+    private Label lbLoggedUser;
+
+    @FXML
+    private MenuItem miPLN;
+
+    @FXML
+    private MenuItem miUSD;
+
+    @FXML
+    private MenuItem miEUR;
+
+    @FXML
+    private MenuItem mi10s;
+
+    @FXML
+    private MenuItem mi1Min;
+
+    @FXML
+    private MenuItem mi10Min;
+    //endregion
+
+    @FXML
+    void currencyMenuOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void updateMenuOnAction(ActionEvent event) {
+
+    }
+}
