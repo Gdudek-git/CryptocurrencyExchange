970d76b2046aba4b14c499810fde37a0ffbdd9f2
Grzegorz Dudek <74261917+MeZziK@users.noreply.github.com>
2021-06-01 22:58:22 +0200

    finished BuyView 1 version

---
+++ src/main/java/ui/controllers/BuyViewController.java
@@ -0,0 +1,54 @@
+package main.java.ui.controllers;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class BuyViewController {
+
+    @FXML
+    private ComboBox cbxCryptocurrency;
+
+    @FXML
+    private ComboBox cbxCurrency;
+
+    @FXML
+    private TextField tfCurrencyAmount;
+
+    @FXML
+    private TextField tfCryptocurrencyAmount;
+
+    @FXML
+    private TextField tfCurrencyPriceInfo;
+
+    @FXML
+    public void initialize()
+    {
+        setComboBoxItems();
+    }
+
+
+
+    @FXML
+    void btnBuyOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnReturnOnClick(ActionEvent event) {
+
+    }
+
+    private void setComboBoxItems()
+    {
+        ObservableList<String> currency = FXCollections.observableArrayList("PLN","USD","EUR");
+        cbxCurrency.setItems(currency);
+        ObservableList<String> cryptocurrency = FXCollections.observableArrayList("BTC","ETH","DOGE");
+        cbxCryptocurrency.setItems(currency);
+
+    }
+
+}
