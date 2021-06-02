808c60cb5efdfa1f9c30f526389390e976d80c6f
Grzegorz Dudek <74261917+MeZziK@users.noreply.github.com>
2021-06-02 10:03:11 +0200

    added SellView 1 version

---
+++ src/main/java/ui/controllers/SellViewController.java
@@ -0,0 +1,53 @@
+package main.java.ui.controllers;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class SellViewController {
+
+    @FXML
+    private ComboBox cbxCryptocurrency;
+
+    @FXML
+    private ComboBox cbxCurrency;
+
+    @FXML
+    private TextField tfCurrencyAmount;
+
+    @FXML
+    private TextField tfCryptocurrencyAmount;
+
+    @FXML
+    private TextField tfCurrencyPriceInfo;
+
+    @FXML
+    public void initialize()
+    {
+        setComboBoxItems();
+    }
+
+    @FXML
+    void btnReturnOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSellOnAction(ActionEvent event) {
+
+    }
+
+    private void setComboBoxItems()
+    {
+        ObservableList<String> currency = FXCollections.observableArrayList("PLN","USD","EUR");
+        cbxCurrency.setItems(currency);
+        ObservableList<String> cryptocurrency = FXCollections.observableArrayList("BTC","ETH","DOGE");
+        cbxCryptocurrency.setItems(currency);
+
+    }
+
+
+}
