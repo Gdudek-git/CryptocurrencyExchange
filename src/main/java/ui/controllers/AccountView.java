ecf79045d1dd538e26e803f0a1be5118a7296380
Grzegorz Dudek <74261917+MeZziK@users.noreply.github.com>
2021-06-03 20:22:19 +0200

    added AccountView 1 version

---
+++ src/main/java/ui/controllers/AccountView.java
@@ -0,0 +1,62 @@
+package main.java.ui.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class AccountView {
+
+    //region Controllers
+    @FXML
+    private Label lbBtcAmount;
+
+    @FXML
+    private Label lbDogeAmount;
+
+    @FXML
+    private Label lbEurAmount;
+
+    @FXML
+    private Label lbEthAmount;
+
+    @FXML
+    private Label lbPlnAmount;
+
+    @FXML
+    private Label lbUsdAmount;
+
+    @FXML
+    private Label lbUsername;
+
+    @FXML
+    private Label lbFirstName;
+
+    @FXML
+    private Label lbLastName;
+
+    @FXML
+    private Label lbEmail;
+
+    @FXML
+    private Label lbNumber;
+
+    @FXML
+    private Label lbCountry;
+    //endregion
+
+    @FXML
+    void btnChangePasswordOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnChangePersonalDataOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnReturnOnAction(ActionEvent event) {
+
+    }
+
+}
