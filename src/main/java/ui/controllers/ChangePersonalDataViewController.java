65155f2f752ea8bdfc13ca655c41e703917adc42
Grzegorz Dudek <74261917+MeZziK@users.noreply.github.com>
2021-06-03 22:24:49 +0200

    added ChangePersonalDataView 1 version

---
+++ src/main/java/ui/controllers/ChangePersonalDataViewController.java
@@ -0,0 +1,63 @@
+package main.java.ui.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class ChangePersonalDataViewController {
+
+    //region Controllers
+    @FXML
+    private TextField tfNewUsername;
+
+    @FXML
+    private TextField tfNewFirstName;
+
+    @FXML
+    private TextField tfNewLastName;
+
+    @FXML
+    private TextField tfNewNumber;
+
+    @FXML
+    private TextField tfNewEmail;
+
+    @FXML
+    private TextField tfNewCountry;
+    //endregion
+
+    @FXML
+    void btnChangeCountryOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnChangeEmailOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnChangeFirstNameOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnChangeLastNameOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnChangeNumberOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnChangeUsernameOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnReturnOnAction(ActionEvent event) {
+
+    }
+}
