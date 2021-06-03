de23c5dd55cf6f8506b0f6078264fa355739cee7
Grzegorz Dudek <74261917+MeZziK@users.noreply.github.com>
2021-06-03 20:44:15 +0200

    added ChangePasswordView 1 version

---
+++ src/main/java/ui/controllers/ChangePasswordViewController.java
@@ -0,0 +1,30 @@
+package main.java.ui.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+
+public class ChangePasswordViewController {
+
+    //region Controllers
+    @FXML
+    private PasswordField pfPassword;
+
+    @FXML
+    private PasswordField pfNewPassword;
+
+    @FXML
+    private PasswordField pfConfirmedNewPassword;
+    //endregion
+
+    @FXML
+    void btnConfirmOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnReturnOnAction(ActionEvent event) {
+
+    }
+
+}
