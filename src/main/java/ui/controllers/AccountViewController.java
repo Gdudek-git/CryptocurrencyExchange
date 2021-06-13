81f510edc622771c833cf1d6db8f7a77444a6413
Grzegorz Dudek <74261917+MeZziK@users.noreply.github.com>
2021-06-13 21:30:12 +0200

    added showing user info in AccountView

--- src/main/java/ui/controllers/AccountViewController.java
+++ src/main/java/ui/controllers/AccountViewController.java
@@ -1,8 +1,10 @@
 package ui.controllers;
 
+import database.entity.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import session.LoggedUser;
 
 import static ui.controllers.MainStage.getScene;
 
@@ -47,6 +49,12 @@ public class AccountViewController {
     private Label lbCountry;
     //endregion
 
+
+    @FXML
+    private void initialize() {
+        setUserAccountInfoLabels();
+    }
+
     @FXML
     void btnChangePasswordOnAction(ActionEvent event) {
         getMainStage().setScene(getScene("ChangePasswordView.fxml"));
@@ -67,4 +75,37 @@ public class AccountViewController {
         return MainStage.getInstance();
     }
 
+
+    private void setUserAccountInfoLabels()
+    {
+        User user = LoggedUser.getInstance().getLoggedUser();
+        setObtainedCryptocurrency(user);
+        setObtainedCurrency(user);
+        setUserData(user);
+
+    }
+
+    private void setUserData(User user)
+    {
+        lbUsername.setText(user.getUsername());
+        lbFirstName.setText(user.getFirstName());
+        lbLastName.setText(user.getLastName());
+        lbCountry.setText(user.getUserContact().getCountry());
+        lbEmail.setText(user.getUserContact().getEmail());
+        lbNumber.setText(user.getUserContact().getPhoneNumber());
+    }
+
+    private void setObtainedCryptocurrency(User user)
+    {
+        lbBtcAmount.setText(String.valueOf(user.getUserWallet().getBtc()));
+        lbEthAmount.setText(String.valueOf(user.getUserWallet().getEth()));
+        lbDogeAmount.setText(String.valueOf(user.getUserWallet().getDoge()));
+    }
+
+    private void setObtainedCurrency(User user)
+    {
+        lbEurAmount.setText(String.valueOf(user.getUserWallet().getEur()));
+        lbPlnAmount.setText(String.valueOf(user.getUserWallet().getPln()));
+        lbUsdAmount.setText(String.valueOf(user.getUserWallet().getUsd()));
+    }
 }
