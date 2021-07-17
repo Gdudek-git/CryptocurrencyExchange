3503bbb32027f7c51baabb5396e398bb8f1af2ab
Grzegorz Dudek <74261917+MeZziK@users.noreply.github.com>
2021-07-17 19:04:39 +0200

    add class containing all available views

---
+++ src/main/java/view/AvailableViews.java
@@ -0,0 +1,16 @@
+package view;
+
+public class AvailableViews {
+
+    public static final String ACCOUNT_VIEW = "AccountView.fxml";
+    public static final String BUY_VIEW = "BuyView.fxml";
+    public static final String CHANGE_PASSWORD_VIEW = "ChangePasswordView.fxml";
+    public static final String CHANGE_PERSONAL_DATA_VIEW = "ChangePersonalDataView.fxml";
+    public static final String EXCHANGE_VIEW = "ExchangeView.fxml";
+    public static final String LOGIN_VIEW = "LoginView.fxml";
+    public static final String MAIN_MENU_VIEW = "MainMenuView.fxml";
+    public static final String REGISTER_VIEW = "RegisterView.fxml";
+    public static final String SELL_VIEW = "SellView.fxml";
+    public static final String SEND_VIEW = "SendView.fxml";
+
+}
